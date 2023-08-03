5be9a9004c194bb56baf56e054391a1792df1cdb
Akash Belekar <akash.belekar@mobifilia.com>
2023-08-03 14:26:35 +0530

    This is 3D Imges

---
+++ Purches3D/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Purches3D
+//
+//  Created by Akash Belekar on 03/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
